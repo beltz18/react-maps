c1246f8103094cdfd8581ace08f1183fac65fa0e
Beltz <beltz.18kyodai@gmail.com>
2022-07-15 15:10:09 -0400

    first commit react-maps

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Maps from './component/main/App'
+import './style/main.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <Maps />
+  </React.StrictMode>
+)
