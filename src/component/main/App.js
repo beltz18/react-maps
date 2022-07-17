94fefa7b8413a86693b46471c6ca1610524f54c8
Beltz <beltz.18kyodai@gmail.com>
2022-07-17 11:15:08 -0400

    first map with React Leaflet v1

--- src/component/main/App.js
+++ src/component/main/App.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
-export default class Maps extends Component {
+export default class MyMap extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Putos</h1>
-      </div>
+      <MapContainer
+        center={[5.725,-72.940]}
+        zoom={18}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        />
+      </MapContainer>
     )
   }
 }
